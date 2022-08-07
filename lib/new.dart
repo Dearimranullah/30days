b5e8167f292b5dbdb8f23289772789a88987ae34
Imranullah <Imranullahym@gmail.com>
2022-08-07 15:20:27 +0500

    New pages added

---
+++ lib/new.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Anayat extends StatelessWidget {
+  const Anayat({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Column(children: const []));
+  }
+}
